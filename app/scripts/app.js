969839de90c047cdad20bfe2fb24fec354508be5
Leigh Hunt <leigh@venari.co.nz>
2015-05-06 09:17:04 +1200

    Graph showing

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -11,6 +11,7 @@
 angular
   .module('angularChartjsPerfIssueApp', [
     'ngAnimate',
+    'chart.js',
     'ngCookies',
     'ngResource',
     'ngRoute',
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,9 +9,37 @@
  */
 angular.module('angularChartjsPerfIssueApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+
+  	$scope.data = [
+  		[1,2,3,4,5,6,7,8,9,10],
+  		[1,2,4,4,5,6,7,8,9,10],
+  		[1,2,5,4,5,6,7,8,9,10],
+  		[1,2,6,4,5,6,7,8,9,10],
+  		[1,2,7,4,5,6,7,8,9,10],
+  		[1,2,8,4,5,6,7,8,9,10],
+  		[1,2,9,4,5,6,7,8,9,10],
+  		[1,2,10,4,5,6,7,8,9,10],
+  		[1,2,1,4,5,6,7,8,9,10],
+  		[1,2,2,4,5,6,7,8,9,10],
+  	];
+
+    $scope.index = 0;
+		$scope.voltages = [];
+    $scope.voltageLabels = ['A','B','C','D','E','F','G','H','I','J'];
+
+  	$scope.voltages[0] = $scope.data[++$scope.index];
+
+
+		setInterval(function () {
+	  	console.log('Tick');
+
+	  	if($scope.index>$scope.data.length){
+	  		$scope.index = 0;
+	  	}
+
+	  	$scope.voltages[0] = $scope.data[++$scope.index];
+	  	//console.log($scope.voltages);
+
+		}, 1000);
+
   });
