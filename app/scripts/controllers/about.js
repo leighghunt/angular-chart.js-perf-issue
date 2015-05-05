9a6094a553457c210a97197bfb7b848e72feb7fa
Leigh Hunt <leigh@venari.co.nz>
2015-05-06 07:52:01 +1200

    yo angular

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name angularChartjsPerfIssueApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the angularChartjsPerfIssueApp
+ */
+angular.module('angularChartjsPerfIssueApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
